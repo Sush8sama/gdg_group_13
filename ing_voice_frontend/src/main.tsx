3cfa72ba22fcdcec60035091234477a310828165
Luna Mei Baeyens <lunamei.baeyens@student.kuleuven.be>
2025-10-29 15:04:54 +0100

    select a specific user

--- ing_voice_frontend/src/main.tsx
+++ ing_voice_frontend/src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import Test from './Test.tsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <Test />
+    <App />
+    {/* <Test /> */}
   </StrictMode>,
 )
